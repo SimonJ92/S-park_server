40b7c19d0f28b1d2d5259ed09acb9ddc8427f8f4
Simon <simon.jolly@edu.ece.fr>
2021-02-26 17:52:36 +0100

    attempt 5

--- test/mysql_pool.js
+++ test/mysql_pool.js
@@ -2,20 +2,16 @@ const { expect } = require('chai')
 const attemptConnection = require('../src/attemptConnection')
 let pool
 
-before(() => {
-  pool = require('../src/mysql_pool')
-})
-
-after(async () => {
-    await pool.end()
-    console.log("Pool ending")
-})
-
-describe('MySQL', () => {
+describe('MySQL', async () => {
+  before(() => {
+    pool = require('../src/mysql_pool')
+  })
+  
   it('should connect to MySQL', () => {
     attemptConnection('SELECT 1 + 1 AS solution', (error, results, fields) => {
       if (error) throw error;
       expect(results[0].solution).to.eql(2)
+      pool.end()
     })
   })
 })
